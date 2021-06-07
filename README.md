8bff7a4a0be3816771eb320ab00db9107b4df3b6
ZhichenRen <1337795211@qq.com>
2021-06-07 23:33:52 +0800

    Update README

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 ## 项目介绍
 
-​		本项目为一个内存调页模拟程序。在本项目所模拟的系统中，执行程序共有320条指令。一页可以存放10条指令，所有指令被分为32页，系统中共有4个页框可供程序使用。本项目将展示程序执行过程中的换页过程，以及缺页率的统计。
+本项目为一个内存调页模拟程序。在本项目所模拟的系统中，执行程序共有320条指令。一页可以存放10条指令，所有指令被分为32页，系统中共有4个页框可供程序使用。本项目将展示程序执行过程中的换页过程，以及缺页率的统计。
 
 
 
@@ -16,13 +16,62 @@ yarn install
 yarn serve
 ```
 
-​		本项目已在Github Page上部署，<a href="zhichenren.github.io/Memory-Management" style="text-decoration: none;font-weight: bold; color: black;">点击访问</a>。
+本项目已在Github Page上部署，<a href="zhichenren.github.io/Memory-Management" style="text-decoration: none;font-weight: bold; color: black;">点击访问</a>。
 
 ## 功能说明
 
+​		本项目实现了如下功能：
 
+- 指令执行条数查看
+- 下一条指令地址查看
+- 单步执行指令
+- 连续执行指令
+- 指令命中提示
+- 缺页提示
+- 缺页率统计
+
+<img src="assets/all.png"></img>
+
+<center>总览</center>
+
+<img src="assets/hit.png" />
+
+<center>指令在页中</center>
+
+<img src="assets/miss.png"/>
+
+<center>缺页提示</center>
+
+<img src="assets/continue.png" />
+
+<center>连续执行</center>
 
 ## 实现介绍
 
+本项目的核心部分是后继指令地址的生成与替换算法的实现，下面简要说明。
 
+### 后继指令生成
 
+本项目遵循50%顺序执行下一条指令，25%执行当前指令前的指令，25%执行当前指令后的指令的指令分布，采用随机的方式执行指令。
+
+```mermaid
+graph TD
+A(开始) --> B(生成0-1间随机数x) --> if{ 判断随机数大小 }
+if --->|0.5 <= x < 0.75| f2[随机向后执行]
+if --->|0 < x < 0.5| f1[顺序执行执行下一条指令]
+if --->|0.75 <= x < 1| f3[随机向前执行]
+```
+
+### FIFO算法实现
+
+程序中维护一个数组 ***call_in_time*** ，用于存放每个页框中的页的换入时间戳，当发生缺页时，程序遍历每一个页框，选择时间戳最小，也就是最早调入的页换出。
+
+### LRU算法实现
+
+程序中维护一个数组 ***priority*** 来表示各页的换出优先级，数值较大的页优先被换出，具体更新策略如下：
+
+- 当一个新的页被换入时，更新其 ***priority*** 为0
+- 当一个已被调入的页被访问时，更新其 ***priority*** 为0
+- 每次新执行一条指令时，令所有被调入的页的 ***priority*** 增加1
+
+从上述更新策略中可以看出，长时间未访问的页的 ***priority*** 值较大，而近期换入或访问的页的 ***priority*** 值较小，在进行替换时前者更有可能被换出。
--- src/components/Operation.vue
+++ src/components/Operation.vue
@@ -1,106 +1,271 @@
 <template>
-    <div class="container px-6 mx-auto border-black border-2 rounded-2xl text-center">
-        <div class="px-0.5 mx-auto mb-3 mt-3">
-            <div class="text-xl mb-3 py-2 max-w-xs mx-auto bg-gray-300 shadow-md items-center rounded-xl">
-                已执行{{ instruction_number }}条指令
-            </div>
-            <div class="text-xl mb-3 py-2 max-w-xs mx-auto bg-gray-300 shadow-md items-center rounded-xl">
-                指令地址<br>{{ instruction_address==-1?'无已执行指令':instruction_address }}
-            </div>
-        </div>
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-5 rounded-full mb-3"
-        @click="this.$emit('set_algorithm','FIFO');">
-            FIFO
-        </button>
-        <br>
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-5 rounded-full mb-3"
-        @click="this.$emit('set_algorithm','LRU');">
-            LRU
-        </button>
-        <!-- <div class="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3" role="alert">
-            <p class="font-bold">Informational message</p>
-            <p class="text-sm">Some additional text to explain said message.</p>
-        </div> -->
-        <div class="text-xl mb-3 mx-auto">
-            当前算法:{{ algorithm }}
-        </div>
-        <button class="text-white font-bold py-1 px-5 rounded-full mb-3"
-        @click="executeOnce" :disabled="execute_type=='停止执行'"
-        :class="{'bg-blue-500 hover:bg-blue-700':execute_type=='连续执行','bg-gray-300':execute_type=='停止执行'}">
-            单条执行
-        </button>
-        <br>
-        <button class=" text-white font-bold py-1 px-5 rounded-full mb-3"
-        :class="{'bg-blue-500 hover:bg-blue-700':execute_type=='连续执行','bg-red-500 hover:bg-red-700':execute_type=='停止执行'}"
-        @click="continueExecute">
-            {{ execute_type }}
-        </button>
-        <div class="text-xl mb-3 py-2 max-w-xs mx-auto bg-gray-300 shadow-md items-center rounded-xl">
-            换页率：{{ missing_rate }}%
-        </div>
-        <button class="text-white font-bold py-1 px-5 rounded-full mb-3"
-        @click="this.$emit('reset');" :disabled="execute_type=='停止执行'"
-        :class="{'bg-blue-500 hover:bg-blue-700':execute_type=='连续执行','bg-gray-300':execute_type=='停止执行'}">
-            复位
-        </button>
+  <div
+    class="container px-6 mx-auto border-black border-2 rounded-2xl text-center"
+  >
+    <div class="px-0.5 mx-auto mb-3 mt-3">
+      <div
+        class="
+          text-xl
+          mb-3
+          py-2
+          max-w-xs
+          mx-auto
+          bg-gray-300
+          shadow-md
+          items-center
+          rounded-xl
+        "
+      >
+        已执行{{ instruction_number }}条指令
+      </div>
+      <div
+        class="
+          text-xl
+          mb-3
+          py-2
+          max-w-xs
+          mx-auto
+          bg-gray-300
+          shadow-md
+          items-center
+          rounded-xl
+        "
+      >
+        下一条指令地址<br />{{
+          instruction_address == -1 ? "无已执行指令" : instruction_address
+        }}
+      </div>
     </div>
+    <button
+      class="
+        bg-blue-500
+        hover:bg-blue-700
+        text-white
+        font-bold
+        py-1
+        px-5
+        rounded-full
+        mb-3
+      "
+      @click="this.$emit('set_algorithm', 'FIFO')"
+    >
+      FIFO
+    </button>
+    <br />
+    <button
+      class="
+        bg-blue-500
+        hover:bg-blue-700
+        text-white
+        font-bold
+        py-1
+        px-5
+        rounded-full
+        mb-3
+      "
+      @click="this.$emit('set_algorithm', 'LRU')"
+    >
+      LRU
+    </button>
+    <div class="text-xl mb-3 mx-auto">当前算法:{{ algorithm }}</div>
+    <button
+      class="text-white font-bold py-1 px-5 rounded-full mb-3"
+      @click="executeOnce"
+      :disabled="execute_type == '停止执行'"
+      :class="{
+        'bg-blue-500 hover:bg-blue-700': execute_type == '连续执行',
+        'bg-gray-300': execute_type == '停止执行',
+      }"
+    >
+      单条执行
+    </button>
+    <br />
+    <div class="grid grid-cols-2">
+      <button
+        v-if="execute_type == '停止执行'"
+        class="
+          bg-blue-500
+          hover:bg-blue-700
+          text-white
+          font-bold
+          py-1
+          px-3
+          rounded-full
+          mb-3
+          mx-1
+        "
+        @click="normalSpeed"
+      >
+        ×1
+      </button>
+      <button
+        v-if="execute_type == '停止执行'"
+        class="
+          bg-blue-500
+          hover:bg-blue-700
+          text-white
+          font-bold
+          py-1
+          px-3
+          rounded-full
+          mb-3
+          mx-1
+        "
+        @click="twoTimesSpeed"
+      >
+        ×2
+      </button>
+      <button
+        v-if="execute_type == '停止执行'"
+        class="
+          bg-blue-500
+          hover:bg-blue-700
+          text-white
+          font-bold
+          py-1
+          px-3
+          rounded-full
+          mb-3
+          mx-1
+        "
+        @click="fiveTimesSpeed"
+      >
+        ×5
+      </button>
+      <button
+        v-if="execute_type == '停止执行'"
+        class="
+          bg-blue-500
+          hover:bg-blue-700
+          text-white
+          font-bold
+          py-1
+          px-3
+          rounded-full
+          mb-3
+          mx-1
+        "
+        @click="tenTimesSpeed"
+      >
+        ×10
+      </button>
+    </div>
+    <button
+      class="text-white font-bold py-1 px-5 rounded-full mb-3"
+      :class="{
+        'bg-blue-500 hover:bg-blue-700': execute_type == '连续执行',
+        'bg-red-500 hover:bg-red-700': execute_type == '停止执行',
+      }"
+      @click="continueExecute"
+    >
+      {{ execute_type }}
+    </button>
+    <br />
+    <button
+      class="text-white font-bold py-1 px-5 rounded-full mb-3"
+      @click="this.$emit('reset')"
+      :disabled="execute_type == '停止执行'"
+      :class="{
+        'bg-blue-500 hover:bg-blue-700': execute_type == '连续执行',
+        'bg-gray-300': execute_type == '停止执行',
+      }"
+    >
+      复位
+    </button>
+    <div
+      class="
+        text-xl
+        mb-3
+        py-2
+        max-w-xs
+        mx-auto
+        bg-gray-300
+        shadow-md
+        items-center
+        rounded-xl
+      "
+    >
+      换页率：{{ missing_rate }}%
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "Operation",
-    props: {
-        miss: Number,
-        total: Number,
-        algorithm: String,
-        instruction_address: Number,
-        execute_type: String,
-        instruction_number: Number
-    },
-    data() {
-        return {
-            interval: Object
+  name: "Operation",
+  props: {
+    miss: Number,
+    total: Number,
+    algorithm: String,
+    instruction_address: Number,
+    execute_type: String,
+    instruction_number: Number,
+  },
+  data() {
+    return {
+      interval: Object,
+      period: Number,
+    };
+  },
+  setup() {},
+  created() {
+    this.period = 1000;
+  },
+  methods: {
+    executeOnce: function () {
+      // console.log(this.period);
+      if (this.instruction_number >= 320) {
+        if (this.execute_type == "停止执行") {
+          alert("指令执行完毕！");
+          this.$emit("set_execute_type", "连续执行");
+          clearInterval(this.interval);
         }
+        return;
+      }
+      this.$emit("execute", this.period);
     },
-    setup() {
-        
+    continueExecute: function () {
+      if (this.execute_type == "连续执行") {
+        // alert("开启连续执行！");
+        this.$emit("set_execute_type", "停止执行");
+        this.interval = setInterval(this.executeOnce, this.period);
+      } else {
+        // alert("结束连续执行！");
+        this.$emit("set_execute_type", "连续执行");
+        clearInterval(this.interval);
+        this.period = 1000;
+      }
     },
-    created() {
+    normalSpeed: function () {
+      this.period = 1000;
+      clearInterval(this.interval);
+      this.interval = setInterval(this.executeOnce, this.period);
     },
-    methods: {
-        executeOnce: function(){
-            if(this.instruction_number >= 320){
-                if(this.execute_type == '停止执行'){
-                    alert("指令执行完毕！");
-                    this.$emit('set_execute_type', '连续执行');
-                    clearInterval(this.interval);
-                }
-                return;
-            }
-            this.$emit("execute");
-        },
-        continueExecute: function(){
-            if(this.execute_type == "连续执行"){
-                // alert("开启连续执行！");
-                this.$emit('set_execute_type', '停止执行');
-                this.interval = setInterval(this.executeOnce,1000);
-            }
-            else{
-                // alert("结束连续执行！");
-                this.$emit('set_execute_type', '连续执行');
-                clearInterval(this.interval);
-            }
+    twoTimesSpeed: function () {
+      this.period = 500;
+      clearInterval(this.interval);
+      this.interval = setInterval(this.executeOnce, this.period);
+    },
+    fiveTimesSpeed: function () {
+      this.period = 200;
+      clearInterval(this.interval);
+      this.interval = setInterval(this.executeOnce, this.period);
+    },
+    tenTimesSpeed: function () {
+      this.period = 100;
+      clearInterval(this.interval);
+      this.interval = setInterval(this.executeOnce, this.period);
+    },
+  },
+  computed: {
+    missing_rate: {
+      get() {
+        if (this.total == 0) {
+          return 0;
         }
+        return Math.floor((this.miss / this.total) * 100);
+      },
     },
-    computed: {
-        missing_rate: {
-            get() {
-                if(this.total==0){
-                    return 0;
-                }
-                return Math.floor(this.miss/this.total*100);
-            }
-        }
-    }
-}
+  },
+};
 </script>
